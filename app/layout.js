5f7c0f81eab7c0ec0ac3f7759f24b0f8eecd71c7
Devansh Rajput <devanshrajput032006@gmail.com>
2025-07-22 19:10:29 +0530

    favicon updated

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" /> {/* Or /logo.png if you're using PNG */}
+        <link rel="icon" href="/assets/favicon.svg" /> {/* Or /logo.png if you're using PNG */}
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Raleway:ital,wght@0,100..900;1,100..900&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
           rel="stylesheet"
