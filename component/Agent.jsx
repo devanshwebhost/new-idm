d1aecfba482f39762dc3035735f427454994e0a5
Devansh Rajput <devanshrajput032006@gmail.com>
2025-09-12 17:50:11 +0530

    https://macdabackend.onrender.com changes

--- component/Agent.jsx
+++ component/Agent.jsx
@@ -44,7 +44,7 @@ export default function ChatWidget() {
     setMessages((prev) => [...prev, newMessage]);
     setInput("");
 
-    const res = await fetch("http://localhost:5000/chat", {
+    const res = await fetch("https://macdabackend.onrender.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: input, userKey }),
